c113e3179241d6731c9e9f4507c1a62b38ec90ee
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2021-12-08 20:23:12 -0500

    more style sheet edits

--- itemsIndex.php
+++ itemsIndex.php
@@ -6,10 +6,8 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Londrina+Shadow&display=swap" rel="stylesheet">
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style2.css"
 
         <meta name="viewport" content="width=device-width, initial-scale1.0">
